88ad84b3069f0e513b03f806eef395c578e2d513
Lilia Lomaka <llomaka80@gmail.com>
2022-01-12 14:25:43 +0200

    Перше завдання виконано

--- js/task-01.js
+++ js/task-01.js
@@ -2,3 +2,7 @@ const categories = document.body.querySelectorAll(".item");
 
 
 console.log(`Number of categories: ${categories.length}`);
+categories.forEach(category => {
+  console.log(`Category: ${category.querySelector('h2').textContent}`);
+  console.log(`Elements: ${category.querySelector('ul').children.length}`);
+});
