6c584f291bed13fb3d69dd6bc714b3b7530aba0e
Lilia Lomaka <llomaka80@gmail.com>
2022-01-12 14:52:27 +0200

    Друге завдання виконано

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach(ingredient => {
+  const li = document.createElement("li");
+  li.className = 'item';
+  li.textContent = ingredient;
+  list.appendChild(li);
+});
