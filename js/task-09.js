b9d324e86520044fd0a40a489ea9af63a7f6260c
Lilia Lomaka <llomaka80@gmail.com>
2022-01-25 21:46:25 +0200

    Task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const changeColorButton = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+function changeBodyColor() {
+  const backgroundColor = getRandomHexColor();
+  body.style.backgroundColor = backgroundColor;
+  span.textContent = backgroundColor;
+}
+
+changeColorButton.addEventListener('click', changeBodyColor);
